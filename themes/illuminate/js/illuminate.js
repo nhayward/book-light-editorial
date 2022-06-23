568136e1504f5b028fe35c08cc19b7fe139eb2b9
Nick Hayward <nhayward2011@gmail.com>
2022-06-23 19:42:48 -0400

    adding additional spam checks to contact form

--- themes/illuminate/js/illuminate.js
+++ themes/illuminate/js/illuminate.js
@@ -9,12 +9,12 @@ $(document).ready(function() {
     });
 
 	$('#contact-form').submit(function(e) {
-		$('.phone').each(function() {
-			if ($(this).val() !== "") {
-				e.preventDefault();
-				location.reload();
-			}
-		});
+        if ($('.phone').val() !== "" ||
+            $('[name="entry.1507132880"]').val().toLowerCase() === "henryfling" ||
+            $('[name="entry.1663698665"]').val().toLowerCase().indexOf("https://fling.nanolabs.es/fling") >= 0) {
+            e.preventDefault();
+            window.location.href = "/contact/thankyou";
+        }
 	});
 
 	$('a[target="_blank"]').each(function() {
